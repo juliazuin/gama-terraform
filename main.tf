63091b403b29aa20f8c098e6171f5c93d1628085
Julia Zuin <zuinjulia@hotmail.com>
2021-10-13 14:09:45 -0300

    adicionando variaveis ao arquivo main

--- main.tf
+++ main.tf
@@ -2,15 +2,59 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "web" {
-  subnet_id     = "subnet-0734ecf92f4be11fa"
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
+  ami           = var.ami
+  instance_type = var.tamanho
+  count = var.qtd
   root_block_device {
     encrypted   = true
     kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d" #key managment service (aws) -> awsmanaged keys -> aws/ebs -> copy arn
     volume_size = 8
   }
   tags = {
-    Name = "ec2-julia-tf"
+    Name = var.nome
   }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*"]
+  }
+}
+
+variable "subnet_id" {
+  description = "Digite a subnet: "
+  validation {
+    condition     = length(var.subnet_id) > 7 && substr(var.subnet_id, 0, 7) == "subnet-"
+    error_message = "O valor do subnet_id não é válido, tem que começar com \"subnet-\"."
+  }
+}
+
+variable "ami" {
+  description = "Digite a AMI: "
+  validation {
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "O valor do ami não é válido, tem que começar com \"ami-\"."
+  }
+
+}
+
+variable "tamanho" {
+  description = "Digite o tamanho da maquina (large, micro...): "
+  validation {
+    condition     = length(var.tamanho) > 3 && substr(var.tamanho, 0, 3) == "t2."
+    error_message = "O valor do tamanho não é válido, tem que começar com \"t2.\"."
+  }
+
+}
+
+variable "qtd" {
+  description = "Digite a quantidade de maquinas: "
+}
+
+variable "nome" {
+  description = "Digite o nome da maquina: "
 }
