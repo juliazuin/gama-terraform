bce1f98e3f757ffbb22501463be25ce209970090
Julia Zuin <zuinjulia@hotmail.com>
2021-10-13 13:48:07 -0300

    add comments

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   root_block_device {
     encrypted   = true
-    kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
-    volume_size = 20
+    kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d" #key managment service (aws) -> awsmanaged keys -> aws/ebs -> copy arn
+    volume_size = 8
   }
   tags = {
     Name = "ec2-julia-tf"
